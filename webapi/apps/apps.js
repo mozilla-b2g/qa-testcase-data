f407cb8328c5a7d622b1330a56b6ba16fecc7f83
jds2501 <jsmith@mozilla.com>
2014-01-01 13:55:28 -0500

    Add cleaned up apps test directory to test case data for hosted apps

---
+++ webapi/apps/apps.js
@@ -0,0 +1,27 @@
+﻿var BASE_PATH = 'http://jds2501.github.io/qa-testcase-data/webapi/apps/';
+
+function handleAppRequest(request) {
+  request.onsuccess = function(e) {
+    var app = e.target.result;
+
+    app.onprogress = function() {
+      console.log('onprogress fired with ' + app.progress);
+    };
+
+    alert("Success installing app : " + app.manifest.name);
+  };
+
+  request.onerror = function(e) {
+    alert("Error installing app : " + e.target.error.name);
+  };
+}
+
+function installHostedApp() {
+  var manifest = document.getElementById('hostedAppManifest').value;
+  var request = navigator.mozApps.install(BASE_PATH + manifest);
+  handleAppRequest(request);
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('installHostedApp').onclick = installHostedApp;
+});
