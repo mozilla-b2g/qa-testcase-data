f544b78bcca1256efb3cb7d95c4980401c9510e1
jds2501 <jsmith@mozilla.com>
2014-01-01 22:03:15 -0500

    Added more onXXX handlers for apps

--- webapi/apps/apps.js
+++ webapi/apps/apps.js
@@ -4,15 +4,29 @@ function handleAppRequest(request) {
   request.onsuccess = function(e) {
     var app = e.target.result;
 
-    app.onprogress = function() {
-      console.log('onprogress fired with ' + app.progress);
+    app.ondownloadsuccess = function () {
+      alert('Success downloading app : ' + app.manifest.name);
     };
 
-    alert("Success installing app : " + app.manifest.name);
+    app.ondownloaderror = function () {
+      alert('Error downloading app : ' + app.downloadError);
+    };
+
+    app.ondownloadavailable = function () {
+      console.log('ondownloadavailable fired : ' + app.manifest.name);
+    };
+
+    app.ondownloadapplied = function () {
+      console.log('ondownloadapplied fired : ' + app.manifest.name);
+    };
+
+    app.onprogress = function () {
+      console.log('onprogress fired with ' + app.progress);
+    };
   };
 
-  request.onerror = function(e) {
-    alert("Error installing app : " + e.target.error.name);
+  request.onerror = function (e) {
+    alert('Error installing app : ' + e.target.error.name);
   };
 }
 
