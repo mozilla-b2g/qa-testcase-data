5b406f58ce13b64efa161c3430806974b32fbd88
jds2501 <jsmith@mozilla.com>
2014-01-04 15:38:00 -0800

    Remove unneeded logging

--- webapi/apps/apps.js
+++ webapi/apps/apps.js
@@ -39,7 +39,6 @@ function installHostedApp() {
 
 function installPackagedApp() {
   var miniManifest = document.getElementById('packagedAppMiniManifest').value;
-  console.log(BASE_PATH + miniManifest);
   var request = navigator.mozApps.installPackage(BASE_PATH + miniManifest);
   handleAppRequest(request);
 }
