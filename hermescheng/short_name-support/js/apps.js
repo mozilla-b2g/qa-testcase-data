cbfde7393cf327aa92f8cc6d909ab755e5fb765d
hermescheng <hcheng@mozilla.com>
2015-03-04 14:16:24 +0800

    refactor test apps for easy testing

---
+++ hermescheng/short_name-support/js/apps.js
@@ -0,0 +1,49 @@
+﻿var ORIGIN = window.location.protocol + '//' + window.location.host;
+var BASE_PATH = ORIGIN + '/qa-testcase-data/hermescheng/short_name-support/';
+
+function handleAppRequest(request) {
+  request.onsuccess = function(e) {
+    var app = e.target.result;
+
+    app.ondownloadsuccess = function () {
+      alert('Success downloading app : ' + app.manifest.name);
+    };
+
+    app.ondownloaderror = function () {
+      alert('Error downloading app : ' + app.downloadError.name);
+    };
+
+    app.ondownloadavailable = function () {
+      console.log('ondownloadavailable fired : ' + app.manifest.name);
+    };
+
+    app.ondownloadapplied = function () {
+      console.log('ondownloadapplied fired : ' + app.manifest.name);
+    };
+
+    app.onprogress = function () {
+      console.log('onprogress fired with ' + app.progress);
+    };
+  };
+
+  request.onerror = function (e) {
+    alert('Error installing app : ' + e.target.error.name);
+  };
+}
+
+function installHostedApp(manifest) {
+//  var manifest = document.getElementById('hostedAppManifest').value;
+  var request = navigator.mozApps.install(BASE_PATH + manifest);
+  handleAppRequest(request);
+}
+
+function installPackagedApp(manifest) {
+//  var miniManifest = document.getElementById('packagedAppMiniManifest').value;
+  var request = navigator.mozApps.installPackage(BASE_PATH + miniManifest);
+  handleAppRequest(request);
+}
+
+/*window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('installHostedApp').onclick = installHostedApp;
+  document.getElementById('installPackagedApp').onclick = installPackagedApp;
+});*/
--- hermescheng/short_name-support/js/show_name.js
+++
@@ -1,2 +0,0 @@
-var short_name = "";
-
